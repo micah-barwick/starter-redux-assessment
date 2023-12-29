df1e64e499f18d074aa08c6d0dbde1035cf46428
Micah Barwick <mbarwick@indeed.com>
2023-12-28 19:12:41 -0800

    initial commit: all tasks are done, but suggestions are not being updated.

--- src/features/suggestion/index.js
+++ src/features/suggestion/index.js
@@ -4,20 +4,22 @@ import {
   fetchSuggestion,
   selectError,
   selectLoading,
-  // Task 18: Import the `selectSuggestion()` selector from the suggestion slice
+  selectSuggestion,
 } from './suggestion.slice';
 import './suggestion.css';
 
 export default function Suggestion() {
-  // Task 19: Call useSelector() with the selectSuggestion() selector
-  // The component needs to access the `imageUrl` and `caption` properties of the suggestion object.
+  const suggestion = useSelector(selectSuggestion);
   const loading = useSelector(selectLoading);
   const error = useSelector(selectError);
   const dispatch = useDispatch();
 
+  const imageUrl = suggestion.imageUrl;
+  const caption = suggestion.caption;
+
   useEffect(() => {
     async function loadSuggestion() {
-      // Task 20: Dispatch the fetchSuggestion() action creator
+      dispatch(fetchSuggestion());
     }
     loadSuggestion();
   }, [dispatch]);
@@ -28,11 +30,11 @@ export default function Suggestion() {
   } else if (error) {
     render = <h3>Sorry, we're having trouble loading the suggestion.</h3>;
   } else {
-    // Task 21: Enable the two JSX lines below needed to display the suggestion on the page
+    
     render = (
       <>
-        {/* <img alt={caption} src={imageUrl} />
-        <p>{imageUrl}</p> */}
+        <img alt={caption} src={imageUrl} />
+        <p>{imageUrl}</p>
       </>
     );
   }
